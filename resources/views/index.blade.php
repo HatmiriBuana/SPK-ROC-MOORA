372f1b0baeb9978742d722fb9821ab1e69c5426b
tiara <tiaraoryza111@gmail.com>
2023-06-12 08:30:59 +0700

    mengedit tata letak tabel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -339,7 +339,8 @@
 
             <h3 class="resume-title"> Metode MOORA </h3>
             <div class="resume-item">
-              <h4>Normalisasi</h4>
+              <h4>Normalisasi Kriteria</h4>
+              <p><em>Normalisasi kriteria dengan memangkatkan nilai setiap kriteria dan kemudian diakarkan</em></p>
             <ul>
                <li><p id="N1"></p></li>
                 <li><p id="N2"></p></li>
@@ -349,8 +350,12 @@
                 
             </ul>
 
-              Tabel normalisasi >
-              <table class="table">
+</div>
+            <div class="resume-item">
+              <h4>Normalisasi Alternatif</h4>
+              
+              <p><em>Normalisasi Alternatif dengan membagi nilai matriks dengan normalisasi kriteria</em></p>
+      <table class="table">
 	            <thead>
 		<tr>
 			<th>Alternatif</th>
@@ -444,8 +449,13 @@
 		</tr>
 	</tbody>
 </table>
+              </p>
+            </div>
+         
 
-Tabel normalisasi terbobot
+          <div class="resume-item">
+              <h4>Normalisasi Terbobot</h4>
+              <p><em>Normalisasi terbobot dengan mengalikan normalisasi kriteria dengan bobot ROC</em></p>
               <table class="table">
 	            <thead>
 		<tr>
@@ -540,8 +550,11 @@ Tabel normalisasi terbobot
 		</tr>
 	</tbody>
 </table>
-<div>
-Tabel mencari nilai Yi
+</div>
+
+<div class="resume-item">
+              <h4>Mencari Nilai Yi</h4>
+              <p><em>Menjumlah kriteria Max dan Menjumlah Kriteria Min, Kemudian Max dikurangi Min</em></p>
               <table class="table">
 	    <thead>
 		<tr>
@@ -617,9 +630,19 @@ Tabel mencari nilai Yi
 	</tbody>
 </table>
 
+</div>
+
+<div class="resume-item">
+              <h4>Perangkingan</h4>
+              <p><em>Mencari Nilai Terbesar</em></p>
+              <div>
+
 <ul><li><p id="rank"></p></li></ul>
 
 </div>
+</div>
+</div>
+
 <script type="text/javascript">
    //mattriks keputusan
 $a11 = 2900000; $a12 = 4; $a13 = 90; $a14 = 80; $a15 = 6;
@@ -855,14 +878,7 @@ NilaiYi.reverse();
 document.getElementById("rank").innerHTML = NilaiYi;
 </script>
 
-            </div>
-            <div class="resume-item">
-              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-              
-              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-              <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-            </div>
-          </div>
+            
           </div>
         </div>
         
@@ -870,161 +886,18 @@ document.getElementById("rank").innerHTML = NilaiYi;
 
     </section><!-- End BAGIAN PERHITUNGAN -->
 
-
-
-
-
-    <!-- ======= Testimonials Section ======= -->
-    <section id="testimonials" class="testimonials section-bg">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Testimonials</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-          <div class="swiper-wrapper">
-
-            <div class="swiper-slide">
-              <div class="testimonial-item" data-aos="fade-up">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                <h3>Saul Goodman</h3>
-                <h4>Ceo &amp; Founder</h4>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-item" data-aos="fade-up" data-aos-delay="100">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-item" data-aos="fade-up" data-aos-delay="200">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                <h3>Jena Karlis</h3>
-                <h4>Store Owner</h4>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-item" data-aos="fade-up" data-aos-delay="300">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                <h3>Matt Brandon</h3>
-                <h4>Freelancer</h4>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-item" data-aos="fade-up" data-aos-delay="400">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                <h3>John Larson</h3>
-                <h4>Entrepreneur</h4>
-              </div>
-            </div><!-- End testimonial item -->
-
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
-
-      </div>
-    </section><!-- End Testimonials Section -->
-
-
-
-
     <!-- ======= BAGIAN KESIMPULAN ======= -->
     <section id="contact" class="contact">
       <div class="container">
 
         <div class="section-title">
           <h2>Kesimpulan</h2>
-          <p>Berdasarkan hasil perhitungan pada sistem ini, menunjukkan bahwa alternatif ke 7 yang</p>
+          <p>Berdasarkan hasil perhitungan pada sistem ini, menunjukkan bahwa alternatif ke 7 adalah siswa yang paling membutuhkan dan paling berhak mendapat bantuan. 
+            hasil yang di dapat sama dengan di jurnal rujukan dengan menggunakan metode ROC, maka disimpulkan bahwa perumusan metode kombinasi ini sudah tepat 
+            dan sistem ini juga menunjukkan hasil yang akurat dan selektif. </p>
         </div>
 
         <div class="row" data-aos="fade-in">
-
-          <div class="col-lg-5 d-flex align-items-stretch">
-            <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
-              </div>
-
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>info@example.com</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
-              </div>
-
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-            </div>
-
-          </div>
-
-          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Your Name</label>
-                  <input type="text" name="name" class="form-control" id="name" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="name">Your Email</label>
-                  <input type="email" class="form-control" name="email" id="email" required>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="name">Subject</label>
-                <input type="text" class="form-control" name="subject" id="subject" required>
-              </div>
-              <div class="form-group">
-                <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="10" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div>
 
         </div>
 
